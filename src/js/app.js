9b28ff95fe2fed842ef0d904a92151ba20f037a1
alppacat <lorena.pratts@gmail.com>
2018-08-09 19:18:53 -0500

    Agrega funcion para tomar fotos

--- src/js/app.js
+++ src/js/app.js
@@ -9,3 +9,23 @@ get.addEventListener('click', event => {
   getDate = date.value;
   databases.registro(getName,getDate);
 });
+
+var player = document.getElementById('player');
+var snapshotCanvas = document.getElementById('snapshot');
+var captureButton = document.getElementById('capture');
+
+var handleSuccess = function (stream) {
+  // Attach the video stream to the video element and autoplay.
+  player.srcObject = stream;
+};
+
+captureButton.addEventListener('click', function () {
+  var context = snapshot.getContext('2d');
+  // Draw the video frame to the canvas.
+  context.drawImage(player, 0, 0, snapshotCanvas.width,
+    snapshotCanvas.height);
+    player.style.display = 'none';
+});
+
+navigator.mediaDevices.getUserMedia({ video: true })
+  .then(handleSuccess);
