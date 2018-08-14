79d4f980aaeb6d48a22d8c05ec63788c27c980f9
alppacat <lorena.pratts@gmail.com>
2018-08-13 19:35:03 -0500

    Agrega data de persona elegida por el usuario

--- dashboard-admi/js/main.js
+++ dashboard-admi/js/main.js
@@ -1,6 +1,6 @@
 // Get a reference to the database service
+const btnLogout = document.getElementById('btn-logout');
 const database = firebase.database();
-
 
 const getUserData = () => {
   database.ref().child('visitante').on('value', (snapshot) => {
@@ -9,18 +9,22 @@ const getUserData = () => {
       let name = dataUser[userUid].name;
       let email = dataUser[userUid].email;
       let date = dataUser[userUid].date;
-      printDataUser(name, email, date);
+      let company = dataUser[userUid].company;
+      let companyPerson = dataUser[userUid].companyPerson;
+      printDataUser(name, email, date, company, companyPerson);
     }; 
   });
 };
 getUserData();
 
-const printDataUser = (name, email, date) => {
+const printDataUser = (name, email, date, company, companyPerson) => {
   const root = document.getElementById('root'); 
   const card = document.createElement('div');
   const container = document.createElement('div');
   const namePerson = document.createElement('p');
   const emailPerson = document.createElement('p');
+  const nameCompany = document.createElement('p');
+  const personCompany = document.createElement('p');
   const datePerson = document.createElement('p');
   const photoPerson = document.createElement('img');
   const time = date;
@@ -30,24 +34,29 @@ const printDataUser = (name, email, date) => {
   container.className = 'container-info';
   namePerson.className = 'name-person';
   emailPerson.className = 'email-person';
+  nameCompany.className = 'company';
+  personCompany.className = 'company-person';
   photoPerson.className = 'photo-person';
   datePerson.className = 'date-person';
 
   photoPerson.src = '../assets/place-image.png' ;  
   namePerson.innerHTML = 'Nombre: ' + name;
   emailPerson.innerHTML = 'Asunto: ' + email;
+  namePerson.innerHTML = company;
+  personCompany.innerHTML = companyPerson;
   datePerson.innerHTML = 'Hora de registro: ' + timeToDate;
   container.appendChild(namePerson);
   container.appendChild(emailPerson);
+  container.appendChild(nameCompany);
+  container.appendChild(personCompany);
   container.appendChild(datePerson);
   card.appendChild(photoPerson);
   card.appendChild(container);
   root.appendChild(card);
 };
 
-const btnLogout = document.getElementById('btn-logout');
-
 btnLogout.addEventListener('click', event => {
   firebase.auth().signOut();
   window.location.assign('../index.html');
 });
+
